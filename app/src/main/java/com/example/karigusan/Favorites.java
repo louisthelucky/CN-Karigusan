42c5947d32f9a6f4ba577a2aa6926718ddd83036
Novel World <michael18ricafrente@gmail.com>
2023-11-03 13:04:15 +0800

    Initial commit

--- app/src/main/java/com/example/karigusan/Favorites.java
+++ app/src/main/java/com/example/karigusan/Favorites.java
@@ -3,6 +3,7 @@ package com.example.karigusan;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,9 @@ public class Favorites extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -53,12 +57,14 @@ public class Favorites extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_favorites, container, false);
+        return view;
     }
 }
